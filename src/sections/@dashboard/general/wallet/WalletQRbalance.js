450fd49e4f7e1b407bb804982980b37212663daa
Christian Andres <lulzphantom@Christians-MacBook-Pro.local>
2022-09-21 22:18:52 -0500

    working on qr

--- src/sections/@dashboard/general/wallet/WalletQRbalance.js
+++ src/sections/@dashboard/general/wallet/WalletQRbalance.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import Slider from 'react-slick';
+import QRCode from 'react-qr-code';
 // @mui
 import { styled, useTheme } from '@mui/material/styles';
 import { Box, Typography, Stack, MenuItem, IconButton } from '@mui/material';
@@ -55,11 +55,16 @@ WalletQRbalance.propTypes = {
 
 export default function WalletQRbalance({ list, sx }) {
   const theme = useTheme();
-
+  console.log(list);
   return (
     <RootStyle sx={sx}>
       <Box sx={{ ...shadowStyle }}>
-        <CardItem key={1} card={1} />
+        <CardItem key={list[0].id} card={list[0]} />
+        sdsdd
+        <div style={{ height: 'auto', margin: '0 auto', maxWidth: 64, width: '100%' }}>
+          sdsd
+          <QRCode value="Hola Mundo" size={256} bgColor="#282c34" fgColor="#fff" level="H" />
+        </div>
       </Box>
     </RootStyle>
   );
@@ -87,7 +92,12 @@ function CardItem({ card }) {
 
   return (
     <CardItemStyle>
-      <Box sx={{ position: 'absolute', top: 16, right: 16, zIndex: 9 }}>dd</Box>
+      <Box sx={{ position: 'absolute', top: 16, right: 16, zIndex: 9 }}>
+        <div style={{ height: 'auto', margin: '0 auto', maxWidth: 64, width: '100%' }}>
+          sdsd
+          <QRCode value="Hola Mundo" size={256} bgColor="#282c34" fgColor="#fff" level="H" />
+        </div>
+      </Box>
     </CardItemStyle>
   );
 }
