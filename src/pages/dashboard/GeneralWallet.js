a62287cce1cb9ca96cfaa9af09a8a32f8a1f9457
Christian Andres <lulzphantom@Christians-MacBook-Pro.local>
2022-09-24 18:28:16 -0500

    added logic, ending layout

--- src/pages/dashboard/GeneralWallet.js
+++ src/pages/dashboard/GeneralWallet.js
@@ -1,10 +1,15 @@
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Stack } from '@mui/material';
+import { Grid, Container } from '@mui/material';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+
 // hooks
 import useSettings from '../../hooks/useSettings';
-// _mock_
-import { _bankingContacts, _bankingCreditCard, _bankingRecentTransitions } from '../../_mock';
+// routes
+import { PATH_DASHBOARD, PATH_AUTH } from '../../routes/paths';
+// hooks
+import useAuth from '../../hooks/useAuth';
+import useIsMountedRef from '../../hooks/useIsMountedRef';
 // components
 import Page from '../../components/Page';
 // sections
@@ -12,20 +17,47 @@ import { TronNavigation, WalletQRbalance } from '../../sections/@dashboard/gener
 
 // ----------------------------------------------------------------------
 
+const _tronMock = {
+  currency: {
+    value: 'USDT',
+    label: '(TRON)',
+  },
+  balance: '1,464.356',
+  value: '0XD4FGFGHDHDBDFDGSDFGF...464GHGGHFGHGFDFE3R5',
+};
 export default function GeneralWallet() {
-  const theme = useTheme();
+  const navigate = useNavigate();
 
+  const { user, logout } = useAuth();
+
+  const isMountedRef = useIsMountedRef();
   const { themeStretch } = useSettings();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate(PATH_AUTH.login, { replace: true });
+
+      // if (isMountedRef.current) {
+      //   handleClose();
+      // }
+    } catch (error) {
+      console.error(error);
+      // enqueueSnackbar('Unable to logout!', { variant: 'error' });
+    }
+  };
 
   return (
     <Page title="General: Wallet">
       <Container maxWidth={themeStretch ? false : 'xl'}>
         <Grid container spacing={3}>
-          <Grid item xs={12} md={7}>
-            <WalletQRbalance list={_bankingCreditCard} />
+          <Grid item xs={12} md={8}>
+            <WalletQRbalance tron={_tronMock} />
           </Grid>
-          <Grid item xs={12} md={5}>
-            <TronNavigation link="Income" logout={'eva:diagonal-arrow-left-down-fill'} />
+          <Grid item xs={12} md={4}>
+            <TronNavigation
+              link="https://tronscan.org/#/address/TK31z3tznepy4DRKk7v2vQGkLqZ2Q6yZUW"
+              handleLogout={handleLogout}
+            />
           </Grid>
         </Grid>
       </Container>
