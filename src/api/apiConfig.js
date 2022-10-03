6801fadb9cf2fbe3d2e295f2d74480f6da6f2238
Christian Andres <lulzphantom@Christians-MacBook-Pro.local>
2022-10-03 13:56:07 +0200

    update api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -2,3 +2,11 @@ export const AUTH = {
     login: '/auth/local',
     signUp: '/auth/local/register',
 }
+
+export const REFERRAL = {
+    referralCodes: '/referral-codes',
+}
+
+export const SPOTS = {
+    generalSpots: '/generalspots'
+}
