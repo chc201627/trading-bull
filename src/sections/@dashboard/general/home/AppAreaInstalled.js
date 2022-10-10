a368b8b73aabf8957bbe9da61e41cd83d5b51943
CarlosRobledoM <94917631+CarlosRobledoM@users.noreply.github.com>
2022-10-10 11:59:59 -0500

    Add table resume of invest in months

--- src/sections/@dashboard/general/home/AppAreaInstalled.js
+++ src/sections/@dashboard/general/home/AppAreaInstalled.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { DesktopDatePicker } from '@mui/x-date-pickers';
 import ReactApexChart from 'react-apexcharts';
 // @mui
-import { Card, CardHeader, Box, TextField, Stack } from '@mui/material';
+import { Card, CardHeader, Box, TextField, Stack, Table, TableContainer, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 // components
 import { BaseOptionChart } from '../../../../components/chart';
 import AppTotalInvestments from './AppTotalInvestments';
@@ -23,6 +23,9 @@ export default function AppAreaInstalled({ title, subheader, chartLabels, chartD
 
   const [value, setValue] = useState(new Date('2022-10-06T21:11:54'));
 
+  const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+  const percents = ['+14.1', '-3.5', '+15', '+12', '-1', '+4.2', '+6.7', '-3.9', '-1.5', '+7.1', '+9.4', '+12']
+
   const handleChange = (newValue) => {
     setValue(newValue);
   };
@@ -38,10 +41,10 @@ export default function AppAreaInstalled({ title, subheader, chartLabels, chartD
   });
 
   return (
-    <Card {...other}>
-      <Stack direction={{xs:'column', md:'row'}} alignItems="center">
+    <Card {...other} sx={{ px: 3, py:2 }}>
+      <Stack direction={{xs:'column', md:'row'}} alignItems="center" justifyContent={'space-between'}>
       <AppTotalInvestments/>
-      <Stack direction={{xs:'column', md:'row'}} spacing={1} sx={{mr:3}} >
+      <Stack direction={{xs:'column', md:'row'}} spacing={1} >
       <DesktopDatePicker
           label="Date desktop"
           inputFormat="MM/dd/yyyy"
@@ -59,12 +62,31 @@ export default function AppAreaInstalled({ title, subheader, chartLabels, chartD
         </Stack>
       </Stack>
       {chartData.map((item) => (
-        <Box key={item.year} sx={{ mt: 3, mx: 3 }} dir="ltr">
+        <Box key={item.year} sx={{ mt: 3 }} dir="ltr">
           {item.year === seriesData && (
             <ReactApexChart type="area" series={item.data} options={chartOptions} height={364} />
           )}
         </Box>
       ))}
+      <TableContainer >
+      <Table>
+        <TableHead>
+          <TableRow >
+            {months.map((month) => (
+              <TableCell align='center'> {month} </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {percents.map((value) => (
+            value>0? 
+            <TableCell align='center' sx={{color: 'green'}}>{value}</TableCell> 
+            : 
+            <TableCell align='center' sx={{color:'red'}}>{value}</TableCell>
+            ))}
+        </TableBody>
+      </Table>
+      </TableContainer>
     </Card>
   );
 }
