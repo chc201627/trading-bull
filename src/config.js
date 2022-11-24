5edab6b47c28d53f96fc43c8336f1606c4f6ef2d
Camilo Hurtado <chc201627@gmail.com>
2022-11-24 08:54:39 -0500

    fixed conflict in config file

--- src/config.js
+++ src/config.js
@@ -6,11 +6,7 @@ import { PATH_DASHBOARD } from './routes/paths';
 // API
 // ----------------------------------------------------------------------
 
-<<<<<<< HEAD
 export const HOST_API = process.env.REACT_APP_HOST_API || 'https://aplication.streakbull.com/api/';
-=======
-export const HOST_API = process.env.REACT_APP_HOST_API || 'http://localhost:1337/api/';
->>>>>>> develop
 
 export const FIREBASE_API = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
